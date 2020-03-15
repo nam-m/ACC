9204af5df06002f3fd751a2a5b15b81bce5b359c
Nam Anh Mai <maithenamanh@gmail.com>
2020-03-15 01:35:48 -0700

    added input

--- lq_controller.m
+++ lq_controller.m
@@ -2,15 +2,18 @@
 % Nam Anh Mai 
 
 %% Car inputs
-xl = 30;
-x = 20;
-vl = 30;
-v = 30;
-
-
-%% State-space system
+t = 0:0.5:5; % time interval
 t_hw = 2; % time head-way between lead and host vehicle
 e0 = 1e-5;
+
+xl = 30*t;
+x = 20*t;
+vl = abs(30*sin(t));
+v = abs(20*sin(t));
+
+err = t_hw-(xl-x);
+
+%% State-space system
 A = [0 1 -1; 0 0 0; 0 0 0];
 B = [0 0; 1 0; 0 1];
 C = [-1 0 t_hw; 0 e0 0];
