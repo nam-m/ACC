b46d03cf919c95fd63d610855b9adaf0aa044fde
Nam Anh Mai <maithenamanh@gmail.com>
2020-03-19 21:26:43 -0700

    Fixed UART output error

--- Tiva C/motor.c
+++ Tiva C/motor.c
@@ -13,8 +13,110 @@
 #include "driverlib/pin_map.h"
 #include "driverlib/uart.h"
 #include "driverlib/rom.h"
+#include "UART.h"
+#include "tm4c123gh6pm.h"
 
 #define PWM_FREQUENCY 55
+
+void UARTIntHandler(void);
+char UART_InChar(void);
+void UART_OutChar(char data);
+uint32_t UART_InUDec(void);
+void UART_OutUDec(uint32_t n);
+void UART_Init(void);
+void printString(char * string);
+
+int main(void)
+{
+    volatile uint32_t ui32Load;
+    volatile uint32_t ui32PWMClock;
+    volatile uint32_t ui32Adjust;
+    volatile double duty_cycle;
+
+    //Calculation to produce pulse width of 1.5ms
+    //pulse width = (1/PWM_FREQUENCY)/1000 * ui32Adjust
+    ui32Adjust = 83;
+    duty_cycle = (1000-ui32Adjust)/10;
+
+    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
+    SysCtlPWMClockSet(SYSCTL_PWMDIV_64); //run PWM clock at 40Mhz/64 = 625kHz
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1); //Enable PWM output
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD); //Enable GPIOD
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); //Enable GPIOF
+
+    //Enable UART peripheral
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    //Set Rx/Tx pins as UART pins
+    GPIOPinConfigure(GPIO_PA0_U0RX); //PA0 for Tx
+    GPIOPinConfigure(GPIO_PA1_U0TX); //PA1 for Rx
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    //Configure PD0 as PWM output pin for Module 1 PWM Generator 0 (p.1233)
+    GPIOPinTypePWM(GPIO_PORTD_BASE, GPIO_PIN_0);
+    GPIOPinConfigure(GPIO_PD0_M1PWM0);
+
+    //First 3 lines unlock GPIO commit control register of PF0
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= 0x01;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;
+
+    //Configure PF0 and PF4 as input
+    GPIODirModeSet(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0, GPIO_DIR_MODE_IN);
+    //Configure internal pull-up resistors for PF0 & PF4
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+
+    //Get PWM clock
+    ui32PWMClock = SysCtlClockGet() / 64;
+    //Determine count to be loaded into Load register (count down to 0 so minus 1)
+    ui32Load = (ui32PWMClock / PWM_FREQUENCY) - 1;
+    PWMGenConfigure(PWM1_BASE, PWM_GEN_0, PWM_GEN_MODE_DOWN);
+    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_0, ui32Load);
+
+    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui32Adjust * ui32Load / 1000); //Set pulse width (min resolution = PWM load value/1000)
+    PWMOutputState(PWM1_BASE, PWM_OUT_0_BIT, true);
+    PWMGenEnable(PWM1_BASE, PWM_GEN_0);
+
+    //Get the current config of UART
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 9600,
+        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+    IntMasterEnable(); //enable processor interrupts
+    IntEnable(INT_UART0); //enable UART interrupt
+    // select receiver interrupts (RX) and receiver timeout interrupts (RT)
+    UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+    printString("Enter motor speed:\n\r");
+
+    while(1)
+    {
+//        UART_Init();
+//        PortF_Init();
+        // ui32Adjust: 83 -> 1.5ms, 111 -> 2ms, 56 -> 1ms
+        //Check if SW1 is pressed to decrease pulse width
+//        printString("Enter motor speed:\n\r");
+        if(GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4)==0x00)
+        {
+            ui32Adjust--;
+            if (ui32Adjust < 56)
+            {
+                ui32Adjust = 56; //1ms
+            }
+            PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui32Adjust * ui32Load / 1000);
+        }
+        //Check if SW2 is pressed to increase pulse width
+        if(GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_0)==0x00)
+        {
+            ui32Adjust++;
+            if (ui32Adjust > 111)
+            {
+                    ui32Adjust = 111; //2ms
+            }
+            PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui32Adjust * ui32Load / 1000);
+        }
+        SysCtlDelay(1);
+    }
+}
 
 void UARTIntHandler(void)
 {
@@ -31,21 +133,36 @@ void UARTIntHandler(void)
     }
 }
 
-uint32_t UART_InUDec(void){
-uint32_t number=0, length=0;
-char character;
-  character = UART_InChar();
-  while(character != CR){ // accepts until <enter> is typed
+char UART_InChar(void)
+{
+  while((UART0_FR_R&UART_FR_RXFE) != 0);
+  return((char)(UART0_DR_R&0xFF));
+}
+
+void UART_OutChar(char data)
+{
+  while((UART0_FR_R&UART_FR_TXFF) != 0);
+  UART0_DR_R = data;
+}
+
+uint32_t UART_InUDec(void)
+{
+    uint32_t number=0, length=0;
+    char character;
+    character = UART_InChar();
+    while(character != CR){ // accepts until <enter> is typed
 // The next line checks that the input is a digit, 0-9.
 // If the character is not 0-9, it is ignored and not echoed
-    if((character>='0') && (character<='9')) {
+    if((character>='0') && (character<='9'))
+    {
       number = 10*number+(character-'0');   // this line overflows if above 4294967295
       length++;
       UART_OutChar(character);
     }
 // If the input is a backspace, then the return number is
 // changed and a backspace is outputted to the screen
-    else if((character==BS) && length){
+    else if((character==BS) && length)
+    {
       number /= 10;
       length--;
       UART_OutChar(character);
@@ -55,14 +172,15 @@ char character;
   return number;
 }
 
-void UART_OutUDec(uint32_t n){
+void UART_OutUDec(uint32_t n)
+{
 // This function uses recursion to convert decimal number
 //   of unspecified length as an ASCII string
-  if(n >= 10){
-    UART_OutUDec(n/10);
-    n = n%10;
-  }
-  UART_OutChar(n+'0'); /* n is between 0 and 9 */
+    if(n >= 10){
+        UART_OutUDec(n/10);
+        n = n%10;
+    }
+    UART_OutChar(n+'0'); /* n is between 0 and 9 */
 }
 
 void UART_Init(void)
@@ -85,78 +203,7 @@ void UART_Init(void)
 
 void printString(char * string)
 {
-    while(*string)
-        printChar(*(string++));
-}
-
-int main(void)
-{
-    volatile uint32_t ui32Load;
-    volatile uint32_t ui32PWMClock;
-    volatile uint32_t ui32Adjust;
-    volatile double duty_cycle;
-
-    //Calculation to produce pulse width of 1.5ms
-    //pulse width = (1/PWM_FREQUENCY)/1000 * ui32Adjust
-    ui32Adjust = 83;
-    duty_cycle = (1000-ui32Adjust)/10;
-
-    ROM_SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
-    ROM_SysCtlPWMClockSet(SYSCTL_PWMDIV_64); //run PWM clock at 40Mhz/64 = 625kHz
-
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1); //Enable PWM output
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD); //Enable GPIOD
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); //Enable GPIOF
-
-    //Configure PD0 as PWM output pin for Module 1 PWM Generator 0 (p.1233)
-    ROM_GPIOPinTypePWM(GPIO_PORTD_BASE, GPIO_PIN_0);
-    ROM_GPIOPinConfigure(GPIO_PD0_M1PWM0);
-
-    //First 3 lines unlock GPIO commit control register of PF0
-    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
-    HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= 0x01;
-    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;
-
-    //Configure PF0 and PF4 as input
-    ROM_GPIODirModeSet(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0, GPIO_DIR_MODE_IN);
-    //Configure internal pull-up resistors for PF0 & PF4
-    ROM_GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
-
-    //Get PWM clock
-    ui32PWMClock = SysCtlClockGet() / 64;
-    //Determine count to be loaded into Load register (count down to 0 so minus 1)
-    ui32Load = (ui32PWMClock / PWM_FREQUENCY) - 1;
-    PWMGenConfigure(PWM1_BASE, PWM_GEN_0, PWM_GEN_MODE_DOWN);
-    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_0, ui32Load);
-
-    ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui32Adjust * ui32Load / 1000); //Set pulse width (min resolution = PWM load value/1000)
-    ROM_PWMOutputState(PWM1_BASE, PWM_OUT_0_BIT, true);
-    ROM_PWMGenEnable(PWM1_BASE, PWM_GEN_0);
-
-    while(1)
-    {
-        // ui32Adjust: 83 -> 1.5ms, 111 -> 2ms, 56 -> 1ms
-        //Check if SW1 is pressed to decrease pulse width
-        printString("Enter motor speed:\n\r")
-        if(ROM_GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4)==0x00)
-        {
-            ui32Adjust--;
-            if (ui32Adjust < 56)
-            {
-                ui32Adjust = 56; //1ms
-            }
-            ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui32Adjust * ui32Load / 1000);
-        }
-        //Check if SW2 is pressed to increase pulse width
-        if(ROM_GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_0)==0x00)
-        {
-            ui32Adjust++;
-            if (ui32Adjust > 111)
-            {
-                    ui32Adjust = 111; //2ms
-            }
-            ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui32Adjust * ui32Load / 1000);
-        }
-        ROM_SysCtlDelay(1);
+    while(*string){
+        UART_OutChar(*(string++));
     }
 }
